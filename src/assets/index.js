c41830f3ddefe6c37c5769efafe2aa9da3455bd6
david <david.ortega.pajaro1@gmail.com>
2024-01-16 12:14:00 -0500

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import logo from "./logo.png";
+import github from "./github.png";
+import link from "./linkedin.svg";
+import twitter from "./twitter.svg";
+
+export { logo, github, link, twitter };
